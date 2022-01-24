2f2056634a76161c76092c36709c4136057a30fb
aqilat94 <aqilat94@gmail.com>
2022-01-24 23:35:49 +0800

    Custom register validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Mras\CustomAuth\Classes\CustomRegister;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        CustomRegister::customValidationRules([
+            'name' => 'required|min:6',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8'
+        ]);
+
+        CustomRegister::customRegisterView(function () {
+            return view('custom-view');
+        });
     }
 }
